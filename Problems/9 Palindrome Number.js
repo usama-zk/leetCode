7e34ccd06cc42b94e5b59b69d24e6d8e1c9286a6
Usama <usama.mehmood2002@gmail.com>
2025-09-05 22:44:47 +0500

    9 Palindrome Number

---
+++ Problems/9 Palindrome Number.js
@@ -0,0 +1,17 @@
+var isPalindrome = function (x) {
+  let str = x.toString();
+  if (str.length <= 1) return true;
+  let r = str.length - 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[r]) {
+      if (i > r) return true;
+      else {
+        r--;
+        continue;
+      }
+    } else {
+      return false;
+    }
+  }
+};
+console.log(isPalindrome("-121"));
